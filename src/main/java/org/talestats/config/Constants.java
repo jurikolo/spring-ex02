66c953dd6e49c2dae87ce52bd6e81c216e51d801
Jurijs Kolomijecs <jurikolo@yandex.com>
2014-10-22 13:38:48 +0300

    Added missing constant

--- src/main/java/org/talestats/config/Constants.java
+++ src/main/java/org/talestats/config/Constants.java
@@ -4,6 +4,8 @@ public class Constants {
 
 	//Amount of Pandora cities
 	public final static Integer CITY_COUNT = Integer.valueOf(43);
+	//Amount of Pandora civil cities
+	public final static Integer CIVIL_CITY_COUNT = Integer.valueOf(27);
 	//City extract delay after application is started
 	public final static long START_DELAY = 20000L;
 	//City extract repeat delay
